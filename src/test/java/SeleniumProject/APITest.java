ee7928404d5f2242fa8fc5972081a14ab91a0b3c
jheerekar <jheerekar@gmail.com>
2020-04-15 21:46:28 +1000

    Deleted all steps in develop branch

--- src/test/java/SeleniumProject/APITest.java
+++ src/test/java/SeleniumProject/APITest.java
@@ -8,72 +8,7 @@ public class APITest {
 	
 	public void postman()
 	{
-		System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");
-		System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");	System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");	System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");	System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");	System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");	System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");	System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");System.out.println("This is postman");
-		System.out.println("This is first commit");
-		System.out.println("This is third commit");
+	
 		
 	}
 	
